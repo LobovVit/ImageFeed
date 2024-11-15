69c67a639e21a29a7882c9bd0e6fdc0e1aee19c1
lobov <lobov.vitaliy@gmail.com>
2024-11-15 12:12:23 +0300

    sprint 11; topic 2/7; les4/4

--- ImageFeed/Presentation/AuthViewController.swift
+++ ImageFeed/Presentation/AuthViewController.swift
@@ -6,7 +6,6 @@
 //
 
 import UIKit
-import ProgressHUD
 
 protocol AuthViewControllerDelegate: AnyObject {
     func authViewController(
@@ -52,10 +51,10 @@ final class AuthViewController: UIViewController {
 extension AuthViewController: WebViewViewControllerDelegate {
     
     func webViewViewController(_ vc: WebViewViewController, didAuthenticateWithCode code: String) {
-        ProgressHUD.animate()
+        UIBlockingProgressHUD.show()
         oauth2Service.fetchOAuthToken(code: code, completion: { [weak self] result in
             guard let self else { return }
-                ProgressHUD.dismiss()
+            UIBlockingProgressHUD.dismiss()
                 switch result {
                 case.success(_):
                     delegate?.authViewController(self, didAuthenticateWithCode: code)
