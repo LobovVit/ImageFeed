e19799b33a57371c667b0980ec87a7aa2c40de58
lobov <lobov.vitaliy@gmail.com>
2024-12-16 10:12:49 +0300

    Alert refactor

---
+++ ImageFeed/Alert/AlertModel.swift
@@ -0,0 +1,18 @@
+//
+//  Alert.swift
+//  ImageFeed
+//
+//  Created by Vitaly Lobov on 06.12.2024.
+//
+
+import UIKit
+
+struct AlertModel {
+    let title: String
+    let message: String
+    let buttonText: String
+    let completion: (() -> Void)?
+    var secondButtonText: String? = nil
+    var secondCompletion: (() -> Void)? = {}
+}
+
